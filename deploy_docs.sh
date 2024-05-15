18e497d8aaccb007b867a4a4a9621da5d982d710
Aras Edes <rased27273333@gmail.com>
2024-05-15 23:59:29 +0300

    deploy docs with authentication

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -8,9 +8,12 @@ poetry run sphinx-build -b html docs/source docs/build/html
 cd docs/build/html
 touch .nojekyll
 
-# Initialize a new git repository and force push to the gh-pages branch
 git init
+git config user.name "GitHub Actions"
+git config user.email "actions@github.com"
+
+# Initialize a new git repository and force push to the gh-pages branch
 git add .
 git commit -m "Deploy documentation"
-git remote add origin https://github.com/edesaras/hurst-estimators.git
-git push --force origin master:gh-pages
+git remote add origin https://github.com/${GITHUB_REPOSITORY}.git
+git push --force "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
